a969ad44f0e2c8419932e7c3a99dc4e03d54849c
Ryan <ryansantos.delossantos121@gmail.com>
2023-09-08 12:41:54 -0500

    update reactapp

--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 import Header from "./CommonComponent/HeaderComponent";
-import MyComp from "./CommonComponent/ryanComponent";
 
 export default class Application extends Component{
 
   constructor(props){
     super(props);
+    this.state = {
+      headerValue: "My First React App. Page"
+    }
   }
 
-  //changes here hot loads the content w/o reloading the server
+  changeText = () => {
+    this.setState({
+      headerValue: "New Title MERN Stack"
+    });
+  };
+
+
   render(){
-    let name = "Bob"
-    //JSX - code, html+JS creating a virtual dom
+    let name = "Bob";
     return(
       <>
         <Header/>
-        <h1> My First React Application Page</h1>
+        <h1>{this.state.headerValue}</h1>
+        <button onClick={this.changeText}>Change Text</button>
         <h2>Hello React {name}</h2>
         <br/>
-        <MyComp/>
-      
       </>
     );
   }
