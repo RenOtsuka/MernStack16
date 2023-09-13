2a3d4ab2c3b1a8706ff686991f548f6760f95517
Ryan <ryansantos.delossantos121@gmail.com>
2023-09-13 16:40:13 -0500

    update comp

--- myOwnApp/src/application/Components/ryanComponent.js
+++ myOwnApp/src/application/Components/ryanComponent.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 let MyComp = (props) => {
-  let name = "ryan";
+  let childAge = 25;
+  
   return(
     <>
       <h2>This is my Component</h2>
-      <h3>My name is {name}</h3>
+      <h3>My name is {props.name}</h3>
+      <button onClick={props.callbk(childAge)}>Click to change Age</button>
     </>
   );
 
